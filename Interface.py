75c4dc2ed71ecd2bcf45b5222f7a3ca1f47be1d1
Philippe Carphin <philippe.carphin@canada.ca>
2016-03-06 14:28:01 -0500

    Updated Interface.py to work with the new project classes.
    I'm noting here that the tkFileDialog doesn't run right if the script is run from inside tmux.

--- Interface.py
+++ Interface.py
@@ -1,23 +1,22 @@
-from tkinter import *
-from tkinter import ttk
-from tkinter import filedialog
-import Parser
-    
+import MoveTree
+from Tkinter import *
+from Tkinter import Tk
+import tkFileDialog    
 
 class mainUI:
     def __init__(self):
         self.root = Tk()
-        self.openFileButton= ttk.Button( self.root, text = 'Chose file')
+        self.openFileButton= Button( self.root, text = 'Chose file')
         self.openFileButton.config(command = self.openfile)
         self.openFileButton.pack()
-        self.parser = Parser.Parser()
-    def openfile(self):
-        self.parser.sgf_file.setFilePath(filedialog.askopenfilename())
-        self.parser.sgf_file.openFile()
-        print('File opened')
 
+    def openfile(self):
+        filename = tkFileDialog.askopenfilename()
+        self.tree = MoveTree.Tree(filename)
+        
 if __name__ == "__main__":
     myUI = mainUI()
     myUI.root.mainloop()
+
 
 
