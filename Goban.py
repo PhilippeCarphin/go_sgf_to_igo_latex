1d0806525c8919284a658b9d213208ae14626572
PewMcDaddy <phil103@hotmail.com>
2016-03-26 20:19:20 -0400

    Added int(...) statement to initialisation params

--- Goban.py
+++ Goban.py
@@ -28,8 +28,8 @@ class Goban:
 
     def __init__(self, width , height):
         self.board = {}
-        self.width = width
-        self.height = height
+        self.width = int(width)
+        self.height = int(height)
         self.ko = (0,0)
         self.positionStack = []
         self.moveStack = []
@@ -238,6 +238,9 @@ def moveTreeTest():
 
 if __name__ == "__main__":
     goban = Goban(19,19)
-    l = goban.__getNeighbors__((18,19))
-    print l
+    try:
+        badSize = Goban('bonjour', "bonjour")
+        print "Non-int-able params not detected"
+    except ValueError:
+        print "Things not changeable to int are detected"
 
