d87e50dcf30ea83c4b11c837d68ce2f35b003c1e
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-10-02 22:30:23 -0400

    Added gpl notices to python files

--- cli.py
+++ cli.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python
-""" Copyright 2016, 2017 Philippe Carphin"""
-
 import igo
 import movetree
 import sys
 import getopt
+
+""" Copyright 2016, 2017 Philippe Carphin"""
+
+""" This file is part of go_sgf_to_igo_latex.
+
+go_sgf_to_igo_latex is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+go_sgf_to_igo_latex is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>."""
+
 
 options = "m:f:v:"
 long_opts = ["move=", "find=", "variation="]
