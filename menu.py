e2222999ea88a4340e3475af10b314a6c73b5ae3
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-10-02 21:23:51 -0400

    PEP8'd menu.py

--- menu.py
+++ menu.py
@@ -1,15 +1,16 @@
-import goban
 import movetree
 import os
-import sys
 import igo
-def isInt(string):
+
+
+def is_int(string):
     i = 0
     while i < len(string):
         if string[i] not in '0123456789':
             return False
         i += 1
     return True
+
 
 class Sai:
     """ Command line interface to the beamer class.  This interface is
@@ -30,16 +31,17 @@ class Sai:
     def __init__(self):
         """ Defines the dictionnary of states, sets the initial state and
         creates a BeamerMaker instance """
-        self.states = { 'init':self.introScreen, 'mainMenu':self.mainMenu,\
-                'finished': None, 'findNode':self.trouverNoeud,\
-                'validateFile': self.userValidate, 'saveFile':self.saveFile,\
-                'open':self.chooseFile,'save':self.saveFile,\
-                'findEndNode':self.trouverNoeudFin}
+        self.states = {'init': self.intro_screen, 'mainMenu': self.main_menu,
+                       'finished': None, 'findNode': self.find_node,
+                       'validateFile': self.user_validate, 'saveFile': self.save_file,
+                       'open': self.choose_file, 'save': self.save_file,
+                       'findEndNode': self.find_end_node}
         self.fileS = ''
         self.current = None
         self.end = None
         self.state = 'open'
         self.bm = igo.BeamerMaker()
+        self.tree = None
 
     def __exec__(self):
         """ Main loop of the state machine.  Calls the function (menu)
@@ -47,34 +49,35 @@ class Sai:
         while self.state != 'finished':
             self.states[self.state]()
 
-    def clear(self):
+    @staticmethod
+    def clear():
         """ Clears the screen and displays the heading """
         os.system('cls' if os.name == 'nt' else 'clear')
         print("=============================== IGO-LaTeX v.0.1 =================================")
 
-    def printCurrent(self):
+    def print_current(self):
         """ Displays the current start move """
         print("================================== Current Move ===================================")
         self.current.node_print()
 
-    def printEnd(self):
+    def print_end(self):
         """ Displays the current end move """
         print("==================================== End move =====================================")
         self.end.node_print()
 
-    def clearPrint(self):
+    def clear_print(self):
         """ Clears the screen and displays the current information """
         self.clear()
-        self.printCurrent()
-        self.printEnd()
+        self.print_current()
+        self.print_end()
     
-    def mainMenu(self):
+    def main_menu(self):
         """ Main menu with options for navigating the game tree and creating a
         mainline starting at the current node """
         self.clear()
         self.tree.print_info()
-        self.printCurrent()
-        self.printEnd()
+        self.print_current()
+        self.print_end()
         choix = input(""" >>>> JS, (c'est correct si je t'appelle JS?), je suis pret a produire des
         diagrammes LaTeX vraiment sick pour toi!!
         
@@ -94,8 +97,8 @@ class Sai:
         if choix == '3':
             self.state = 'findEndNode'
 
-    def trouverNoeudFin(self):
-        self.clearPrint()
+    def find_end_node(self):
+        self.clear_print()
         choix = input(""" Whyyyy is the ice slippery ?
 
         Chercher un noeud de fin par
@@ -108,11 +111,11 @@ class Sai:
         if choix == 'A' or choix == 'a':
             self.state = 'mainMenu'
         elif choix == 'C' or choix == 'c':
-            searchString = input(""" Jean-Sebastien, dit moi la chaine de
+            search_string = input(""" Jean-Sebastien, dit moi la chaine de
             caracteres a chercher : """)
-            self.end = self.findnodeFrom(self.tree.head,searchString)
+            self.end = self.find_node_from(self.tree.head, search_string)
         else:
-            if isInt(choix):
+            if is_int(choix):
                 current = self.current
                 i = 1
                 while i < int(choix):
@@ -122,8 +125,8 @@ class Sai:
                         break
                 self.end = current
 
-    def trouverNoeud(self):
-        self.clearPrint()
+    def find_node(self):
+        self.clear_print()
         choix = input(""" Mes systemes sont a la fine pointe de la
         technologie.  J'ai donc plusieurs facons de trouver des noeuds
 
@@ -139,11 +142,11 @@ class Sai:
         #     for child in self.end.children:
         #         child.nodePrint()
         elif choix == 'C' or choix == 'c':
-            searchString = input(""" Jean-Sebastien, dit moi la chaine de
+            search_string = input(""" Jean-Sebastien, dit moi la chaine de
             caracteres a chercher : """)
-            self.current = self.findnodeFrom(self.current,searchString)
+            self.current = self.find_node_from(self.current, search_string)
         else:
-            if isInt(choix):
+            if is_int(choix):
                 current = self.tree.head
                 i = 1
                 while i < int(choix):
@@ -153,12 +156,13 @@ class Sai:
                         break
                 self.current = current
 
-    def findnodeFrom(self,start,string):
+    @staticmethod
+    def find_node_from(start, string):
         ts = movetree.TextSearchVisitor(string)
         start.accept_visitor(ts)
         return ts.get_result()
 
-    def userValidate(self):
+    def user_validate(self):
         self.clear()
         print(self.fileS)
         choix = input(""" >>>> Voici le code genere, est-ce qu'il te plait? 
@@ -172,7 +176,7 @@ class Sai:
         else:
             self.state = 'mainMenu'
 
-    def saveFile(self):
+    def save_file(self):
         self.clear()
         name = input(""" >>>> Super! Je suis content d'avoir pu rencontrer
         tes exigences.
@@ -181,12 +185,12 @@ class Sai:
 
         Nom: """)
 
-        f = open(name,'w')
+        f = open(name, 'w')
         f.write(self.fileS)
         f.close()
         self.state = 'mainMenu'
 
-    def introScreen(self):
+    def intro_screen(self):
         self.clear()
         print("""
         Bonjour Jean-Sebastien, je suis ton assistant Sai, que puis-je faire pour
@@ -196,17 +200,16 @@ class Sai:
 
         """)
         choix = input(" Ton choix :")
-        choix = 'o'
         if choix == 'o' or choix == 'O':
             self.state = 'open'
 
-    def chooseFile(self):
+    def choose_file(self):
         # Get list of files
-        fileList = os.listdir(os.getcwd())
+        file_list = os.listdir(os.getcwd())
 
         # Choose only SGF files
         sgf_files = []
-        for file in fileList:
+        for file in file_list:
             if file.endswith('sgf'):
                 sgf_files.append(file)
 
@@ -219,7 +222,7 @@ class Sai:
         # Write list
         i = 1
         for f in sgf_files:
-            print("        ",i, ': ',f)
+            print("        ", i, ': ', f)
             i += 1
         # Get number from user
         choix = input("""
@@ -233,13 +236,7 @@ class Sai:
         self.end = self.tree.head
         self.state = 'mainMenu'
 
-        self.state = 'mainMenu'
+
 if __name__ == "__main__":
     cyborg = Sai()
     cyborg.__exec__()
-    mt = movetree.Tree('Variations.sgf')
-    bm = igo.BeamerMaker()
-    bm.all_options(mt.head, 'Variations:')
-
-    # bm.ml_to(current)
-        
