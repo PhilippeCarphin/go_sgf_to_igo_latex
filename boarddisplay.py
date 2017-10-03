c5016a9b11b4f0fca4f4c8d9efa27aebd3957279
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-10-03 00:54:28 -0400

    Refactoring boarddisplay.py

--- boarddisplay.py
+++ boarddisplay.py
@@ -35,50 +35,47 @@ class BoardCanvas:
         self.parent = parent
         self.canvas = Canvas(self.parent)
         self.position = {}
-        self.draw_position()
         self.canvas.pack()
         self.parent.bind('<Configure>', lambda e: self.draw_position())
+        self.side_length = 0
+        self.stone_size = 0
+        self.draw_position()
 
     def set_position(self, my_goban):
         self.position = my_goban
 
     def draw_position(self):
         self.canvas.delete('all')
-        # Regler la grandeur du canevas pour qu'il remplisse le parent
-        side_length = min( self.parent.winfo_height(), self.parent.winfo_width()) - 15
-        self.canvas.config( height = side_length, width = side_length)
-        # Calculer les dimensions du board resultant
-        stone_size = (self.cell_size * 23) // 13
-        self.cell_size = side_length // 19
-        # Valeurs pour centrer les cercles sur les intersections
+        self.update_dimensions()
+        self.draw_lines()
+        self.draw_stones()
+
+    def update_dimensions(self):
+        self.side_length = min(self.parent.winfo_height(), self.parent.winfo_width()) - 15
+        self.canvas.config(height=self.side_length, width=self.side_length)
+        self.stone_size = (self.cell_size * 23) // 13
+        self.cell_size = self.side_length // 19
+
+    def draw_stones(self):
+        for goban_coord in self.position:
+            self.draw_stone(goban_coord)
+
+    def draw_stone(self, goban_coord):
+        x = goban_coord[0] * self.cell_size - self.cell_size // 2
+        y = goban_coord[1] * self.cell_size - self.cell_size // 2
+        color = self.position[goban_coord]
         x_offset = 0
         y_offset = 3
-        # Dessiner les lignes verticales du goban
-        for x in range(19):
-            self.canvas.create_line(x * self.cell_size + self.cell_size // 2,
-                                    self.cell_size // 2,
-                                    x * self.cell_size + self.cell_size // 2,
-                                    18 * self.cell_size + self.cell_size // 2)
-        # Dessiner les lignes horizontales du gobank
-        for y in range(19):
-            self.canvas.create_line(self.cell_size // 2,
-                                    y * self.cell_size + self.cell_size // 2,
-                                    18 * self.cell_size + self.cell_size // 2,
-                                    y * self.cell_size + self.cell_size // 2)
-        # Dessiner 
-        for key in self.position:
-            x = key[0]
-            y = key[1]
-            if self.position[key] == 'W':
-                # Dessiner un cercle unicode aux coordonnees ( x, y)
-                self.canvas.create_text(x * self.cell_size - self.cell_size // 2 + x_offset,
-                                        y * self.cell_size - self.cell_size // 2 - y_offset,
-                                        text=u'\u25CB', font=('Arial', stone_size), fill='black')
-            else:
-                # Dessiner un cercle unicode rempli
-                self.canvas.create_text(x * self.cell_size - self.cell_size // 2 + x_offset,
-                                        y * self.cell_size - self.cell_size // 2 - y_offset,
-                                        text=u'\u25CF', font=('Arial', stone_size))
+        text = u'\u25CB' if color == 'W'else u'\u25CF'
+        self.canvas.create_text(x + x_offset, y - y_offset, text=text, font=('Arial', self.stone_size), fill='black')
+
+    def draw_lines(self):
+        max_pos = 18 * self.cell_size + self.cell_size // 2
+        min_pos = self.cell_size // 2
+        for i in range(19):
+            current_dim = i * self.cell_size + self.cell_size // 2
+            self.canvas.create_line(current_dim, min_pos, current_dim, max_pos)
+            self.canvas.create_line(min_pos, current_dim, max_pos, current_dim)
 
 
 if __name__ == "__main__":
@@ -90,7 +87,7 @@ if __name__ == "__main__":
     # root.
     bc = BoardCanvas(root)
 
-    # Charger le fichier d'une partie de Go 
+    # Charger le fichier d'une partie de Go
     moveTree = movetree.Tree("test_files/expected_write_sgf.sgf")
 
     # Naviguer l'arbre jusqu'a la fin
