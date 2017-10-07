705c4f80b9e21f4c918fb4dd1f3355850d7e0f7d
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-10-07 02:48:00 -0400

    Corrected license

--- model.py
+++ model.py
@@ -1,5 +1,21 @@
-from movetree import Move
-from goban import Goban, goban_to_sgf
+from goban import Goban
+
+""" Copyright 2016, 2017 Philippe Carphin"""
+
+""" This file is part of go_sgf_to_igo_latex.
+
+go_sgf_to_igo_latex is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+go_sgf_to_igo_latex is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with go_sgf_to_igo_latex.  If not, see <http://www.gnu.org/licenses/>."""
 
 
 class Model(object):
