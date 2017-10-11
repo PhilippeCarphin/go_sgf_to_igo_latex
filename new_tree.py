cf8d1565c424b1332494946d3e2db998af87bb95
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-10-10 20:57:48 -0400

    Added sgf stuff to nodes

--- new_tree.py
+++ new_tree.py
@@ -1,9 +1,20 @@
 from new_goban import Goban
 
+class Glyphs():
+    def __init__(self):
+        self.circles = []
+        self.squares = []
+        self.triangles = []
+
 class Node(object):
     def __init__(self, parent=None):
         self.children = []
         self.parent = parent
+        self.child_number = 1
+    def add_child(self, node):
+        self.children.append(node)
+        node.parent = self
+        node.child_number = len(self.children)
 
 class Stone(object):
     def __init__(self, color=None, coord=(None,None)):
@@ -14,7 +25,15 @@ class Move(Node, Stone):
     def __init__(self, parent=None, color=None, coord=(None,None)):
         Node.__init__(self, parent)
         Stone.__init__(self, color, coord)
+        self.properties = {}
+        self.glyphs = Glyphs()
         self.position = None
+    def __str__(self):
+        return "M" + str(self.coord) + str(self.properties)
+    def print(self):
+        print(str(self))
+        for c in self.children:
+            c.print()
 
 class MoveTree(object):
     def __init__(self, goban_height=19, goban_width=19):
