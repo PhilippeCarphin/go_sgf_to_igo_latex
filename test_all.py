eb81e947a1288c2634f2ad1c3a3ae9de020d97c5
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-10-13 00:15:52 -0400

    Added licence info to some files

--- test_all.py
+++ test_all.py
@@ -1,9 +1,25 @@
 import unittest
 import os
-
-
 import movetree
 import sgfparser as sgf
+
+""" Copyright 2016, 2017 Philippe Carphin"""
+
+""" This file is part of go_sgf_to_igo_latex.
+
+go_sgf_to_igo_latex is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+go_sgf_to_igo_latex is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with go_sgf_to_igo_latex.  If not, see <http://www.gnu.org/licenses/>."""
+
 
 class test_sgfparser(unittest.TestCase):
     def test_props_from_node_token(self):
