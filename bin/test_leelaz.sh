f04966af8a047125078f919db68edcb19ba5e1ef
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-03-14 05:55:17 -0400

    test

---
+++ bin/test_leelaz.sh
@@ -0,0 +1,14 @@
+pushd $(dirname $0) >/dev/null
+basepath=$(pwd)
+popd > /dev/null
+
+echo "basepath = $basepath"
+
+leela="echo invalid uname"
+if [ $(uname) = Darwin ] ; then
+	leela=./leelaz_osx_x64_opencl
+elif [ $(uname) = Linux ] ; then
+	leela=./leelaz_linux_x64 -w 
+fi
+
+# $leela -w leelaz-model-5309030-128000.txt
